3f5b66d9ce2d9ba731cf94511f6c46f0c1e09789
babak_ss <ssalimib@gmail.com>
2017-02-08 21:41:44 +0330

    Add Game menu & Add XO funciton

--- XO-2Player-Game/XO-2Player-Game/Program.cs
+++ XO-2Player-Game/XO-2Player-Game/Program.cs
@@ -4,6 +4,66 @@ namespace XO_2Player_Game
 {
     class Program
     {
+        static void Main()
+        {
+            int SelectedOption = 0;
+            string input;
+            bool valid = false;
 
+            Console.WriteLine("\n\t****** WELCOME TO X-O ******");
+            Console.Write("\n\tType '1' to start the game with your friend: ");
+            input = Console.ReadLine();
+            Console.WriteLine();
+            Int32.TryParse(input, out SelectedOption);
+
+            while (valid == false)
+            {
+                switch (SelectedOption)
+                {
+                    case 1:
+                        XO();// start game function
+                        valid = true;
+                        Console.WriteLine("OK!");
+                        break;
+                    default:
+                        Console.Write("\tInvalid Input try again: ");
+                        input = Console.ReadLine();
+                        Int32.TryParse(input, out SelectedOption);
+                        break;
+                }
+            }
+
+
+            Console.WriteLine("Finish!");
+
+            Console.ReadLine();
+        }
+
+        static void XO()
+        {
+            Console.Clear();
+            Console.WriteLine("\n\t****** X-O ******");
+
+            int[] PlayerA = new int[5];
+            int[] PlayerB = new int[5];
+            string input;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Player A: ");
+                input = Console.ReadLine();
+                Int32.TryParse(input, out PlayerA[i]);
+
+
+                Console.Write("Player B: ");
+                input = Console.ReadLine();
+                Int32.TryParse(input, out PlayerB[i]);
+            }
+        }
+
+        static bool WinCheck(int[] playerChoices)
+        {
+            return false;
+        }
     }
 }
