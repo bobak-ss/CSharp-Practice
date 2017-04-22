ecdaeffd153fc6efb5474ce61bab77842453ba56
babak-ss <ssalimib@gmail.com>
2017-04-22 20:51:53 +0430

    changing whole game system

--- XO-2Player-Game/XO-2Player-Game/Program.cs
+++ XO-2Player-Game/XO-2Player-Game/Program.cs
@@ -2,6 +2,22 @@
 
 namespace XO_2Player_Game
 {
+    class playGround
+    {
+        public bool?[] node = new bool?[9];
+
+        public bool userInput(bool player, int place)
+        {
+            if (node[place] == null)
+            {
+                node[place] = player;
+                return true;
+            }
+            else
+                return false;
+        }
+        
+    }
     class Game
     {
         /**
@@ -21,73 +37,53 @@ namespace XO_2Player_Game
             Console.Clear();
             Console.WriteLine("\n\t****** X-O ******");
 
-            int[] PlayerA = new int[5];             // player A inputs
-            int[] PlayerB = new int[5];             // player B inputs
-            int[] usedFields = new int[9];         // saves used fields to prevent reusing the field 
-            int counter = 1;                        // usedFields counter
+            playGround plyGrnd = new playGround();
+            int choice;
+            bool valid = false;
             string input;
-
             for (int i = 0; i < 5; i++)
             {
-                Console.Write("Player A: ");
-                input = Console.ReadLine();
-                Int32.TryParse(input, out PlayerA[i]);      // changes input type from string to int
-                bool valid = true;
+                // player A :=: true
                 do
                 {
-                    // checks if inputed field number is used or not.(is filled already?)
-                    for (int j = counter - 1; j >= 0; j--)
+                    Console.Write("Player A: ");
+                    input = Console.ReadLine();
+                    Int32.TryParse(input, out choice);      // changes input type from string to int
+                    if (plyGrnd.userInput(true, choice-1) == false)
                     {
-                        if (usedFields[j] == PlayerA[i])
-                        {
-                            valid = false;
-                            Console.WriteLine("This field is used! try again: ");
-                            input = Console.ReadLine();
-                            Int32.TryParse(input, out PlayerA[i]);
-                            break;
-                        }
-                        else
-                            valid = true;
+                        Console.WriteLine("This field is used! try again: ");
+                        valid = false;
                     }
+                    else
+                        valid = true;
                 } while (valid == false);
-
-                usedFields[counter] = PlayerA[i];   // if the input is OK push it in usedFields
-                counter++;
-                if (this.winCheck(ref PlayerA))              // checks if the player wins it or not in every turn
+                if (check(true, choice-1, plyGrnd))
                 {
                     Console.WriteLine("\n\tPlayer A is the Winner!");
                     break;
                 }
 
-                // everything happened for Player A also happens to Player B
-                Console.Write("Player B: ");
-                input = Console.ReadLine();
-                Int32.TryParse(input, out PlayerB[i]);
-                valid = true;
+                // player B :=: false
                 do
                 {
-                    for (int j = counter - 1; j >= 0; j--)
+                    Console.Write("Player B: ");
+                    input = Console.ReadLine();
+                    Int32.TryParse(input, out choice);      // changes input type from string to int
+                    if (plyGrnd.userInput(false, choice-1) != true)
                     {
-                        if (usedFields[j] == PlayerB[i])
-                        {
-                            valid = false;
-                            Console.Write("This field is used! try again: ");
-                            input = Console.ReadLine();
-                            Int32.TryParse(input, out PlayerB[i]);
-                            break;
-                        }
-                        else
-                            valid = true;
+                        Console.WriteLine("This field is used! try again: ");
+                        valid = false;
                     }
+                    else
+                        valid = true;
                 } while (valid == false);
-                usedFields[counter] = PlayerB[i];
-                counter++;
-                if (this.winCheck(ref PlayerB))
+                if (check(false, choice-1, plyGrnd))
                 {
                     Console.WriteLine("\n\tPlayer B is the Winner!");
                     break;
                 }
             }
+            
         }
 
         /**
@@ -97,175 +93,60 @@ namespace XO_2Player_Game
             @params: an array of player choices to check
             @returns: true if the player wins false if not
         */
-        public bool winCheck(ref int[] playerChoices)
+        public bool check(bool player, int place, playGround plyGrnd)
         {
-            for (int i = 0; i < 5; i++)
+            if (place == 0)
             {
-                if (playerChoices[i] == 1) // *************************** 1
-                {
-                    if (check(ref playerChoices, 2, 3)) // 1, 2, 3
-                        return true;
+                if (plyGrnd.node[1] == player && plyGrnd.node[2] == player)
+                    return true;
+                else if (plyGrnd.node[3] == player && plyGrnd.node[6] == player)
+                    return true;
+                else if (plyGrnd.node[4] == player && plyGrnd.node[8] == player)
+                    return true;
+                else
+                    return false;
 
-                    if (check(ref playerChoices, 3, 2)) // 1, 2, 3
-                        return true;
-
-                    if (check(ref playerChoices, 4, 7)) // 1, 4, 7
-                        return true;
-
-                    if (check(ref playerChoices, 7, 4)) // 1, 7, 4
-                        return true;
-
-                    if (check(ref playerChoices, 5, 9)) // 1, 5, 9
-                        return true;
-
-                    if (check(ref playerChoices, 9, 5)) // 1, 9, 5
-                        return true;
-                }
-
-                if (playerChoices[i] == 3) // *************************** 3
-                {
-                    if (check(ref playerChoices, 2, 1)) // 3, 2, 1
-                        return true;
-
-                    if (check(ref playerChoices, 1, 2)) // 3, 1, 2
-                        return true;
-
-                    if (check(ref playerChoices, 6, 9)) // 3, 6, 9
-                        return true;
-
-                    if (check(ref playerChoices, 9, 6)) // 3, 9, 6
-                        return true;
-
-                    if (check(ref playerChoices, 5, 7)) // 3, 5, 7
-                        return true;
-
-                    if (check(ref playerChoices, 7, 5)) // 3, 7, 5
-                        return true;
-                }
-
-                if (playerChoices[i] == 7) // *************************** 7
-                {
-                    if (check(ref playerChoices, 8, 9)) // 7, 8, 9
-                        return true;
-
-                    if (check(ref playerChoices, 9, 8)) // 7, 9, 8
-                        return true;
-
-                    if (check(ref playerChoices, 4, 1)) // 7, 4, 1
-                        return true;
-
-                    if (check(ref playerChoices, 1, 4)) // 7, 1, 4
-                        return true;
-
-                    if (check(ref playerChoices, 5, 3)) // 7, 5, 3
-                        return true;
-
-                    if (check(ref playerChoices, 3, 5)) // 7, 3, 5
-                        return true;
-                }
-
-                if (playerChoices[i] == 9) // *************************** 9
-                {
-                    if (check(ref playerChoices, 8, 7)) // 9, 8, 7
-                        return true;
-
-                    if (check(ref playerChoices, 7, 8)) // 9, 7, 8
-                        return true;
-
-                    if (check(ref playerChoices, 6, 3)) // 9, 6, 3
-                        return true;
-
-                    if (check(ref playerChoices, 3, 6)) // 9, 3, 6
-                        return true;
-
-                    if (check(ref playerChoices, 5, 1)) // 9, 5, 1
-                        return true;
-
-                    if (check(ref playerChoices, 1, 5)) // 9, 1, 5
-                        return true;
-                }
-
-                if (playerChoices[i] == 2) // *************************** 2
-                {
-                    if (check(ref playerChoices, 5, 8)) // 2, 5, 8
-                        return true;
-
-                    if (check(ref playerChoices, 8, 9)) // 2, 8, 9
-                        return true;
-                }
-
-                if (playerChoices[i] == 4) // *************************** 4
-                {
-                    if (check(ref playerChoices, 5, 6)) // 4, 5, 6
-                        return true;
-
-                    if (check(ref playerChoices, 6, 5)) // 4, 6, 5
-                        return true;
-                }
-
-                if (playerChoices[i] == 6) // *************************** 6
-                {
-                    if (check(ref playerChoices, 5, 4)) // 6, 5, 4
-                        return true;
-
-                    if (check(ref playerChoices, 8, 9)) // 6, 4, 5
-                        return true;
-                }
-
-                if (playerChoices[i] == 8) // *************************** 8
-                {
-                    if (check(ref playerChoices, 5, 2)) // 8, 5, 2
-                        return true;
-
-                    if (check(ref playerChoices, 8, 9)) // 8, 2, 5
-                        return true;
-                }
-
-                if (playerChoices[i] == 5) // *************************** 5
-                {
-                    if (check(ref playerChoices, 2, 8)) // 5, 2, 8
-                        return true;
-
-                    if (check(ref playerChoices, 8, 2)) // 5, 8, 2
-                        return true;
-
-                    if (check(ref playerChoices, 4, 6)) // 5, 4, 6
-                        return true;
-
-                    if (check(ref playerChoices, 6, 4)) // 5, 6, 4
-                        return true;
-
-                    if (check(ref playerChoices, 1, 9)) // 5, 1, 9
-                        return true;
-
-                    if (check(ref playerChoices, 6, 4)) // 5, 9, 1
-                        return true;
-
-                    if (check(ref playerChoices, 6, 4)) // 5, 3, 7
-                        return true;
-
-                    if (check(ref playerChoices, 6, 4)) // 5, 7, 3
-                        return true;
-                }
             }
-            return false;
-        }
+            else if (place == 6)
+            {
+                if (plyGrnd.node[3] == player && plyGrnd.node[0] == player)
+                    return true;
+                else if (plyGrnd.node[7] == player && plyGrnd.node[8] == player)
+                    return true;
+                else if (plyGrnd.node[4] == player && plyGrnd.node[2] == player)
+                    return true;
+                else
+                    return false;
 
-        /**
-            checks given wining scenario
+            }
+            else if (place == 2)
+            {
+                if (plyGrnd.node[1] == player && plyGrnd.node[0] == player)
+                    return true;
+                else if (plyGrnd.node[5] == player && plyGrnd.node[8] == player)
+                    return true;
+                else if (plyGrnd.node[4] == player && plyGrnd.node[6] == player)
+                    return true;
+                else
+                    return false;
 
-            @params: array of player choices
-            @returns: true if the scenario exist and given player is the winner
-        */
-        public bool check(ref int[] choices, int secondChoice, int thirdChoice)
-        {
-            for (int i = 0; i < 5; i++)
-                if (choices[i] == secondChoice)
-                    for (int j = 0; j < 5; j++)
-                        if (choices[j] == thirdChoice)
-                            return true;
-            return false;
+            }
+            else if (place == 8)
+            {
+                if (plyGrnd.node[6] == player && plyGrnd.node[7] == player)
+                    return true;
+                else if (plyGrnd.node[5] == player && plyGrnd.node[2] == player)
+                    return true;
+                else if (plyGrnd.node[4] == player && plyGrnd.node[0] == player)
+                    return true;
+                else
+                    return false;
+
+            }
+            else
+                return false;
         }
+        
     }
     class Program
     {
